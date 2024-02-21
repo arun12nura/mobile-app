e811b9c15299bcf59bb3ce376b8cfaedbf7d2cb4
arun12nura <55918280+arun12nura@users.noreply.github.com>
2024-02-21 21:26:05 +0530

    Update log_in_screen.dart

--- lib/presentation/log_in_screen/log_in_screen.dart
+++ lib/presentation/log_in_screen/log_in_screen.dart
@@ -87,16 +87,16 @@ class LogInScreen extends StatelessWidget {
                   Padding(
                     padding: EdgeInsets.symmetric(horizontal: 30.h),
                     child: CustomTextFormField(
-  controller: mobileController,
-  hintText: "Mobile",
-  textInputType: TextInputType.phone,
-  borderDecoration: InputDecoration(
-    contentPadding: EdgeInsets.symmetric(horizontal: 30.h, vertical: 16.v),
-    border: OutlineInputBorder(
-      borderSide: BorderSide(color: Colors.black), // Specify the border color
-    ),
-  ),
-  fillColor: appTheme.whiteA700,
+                      controller: mobileController,
+                      hintText: "Mobile",
+                      textInputType: TextInputType.phone,
+                      borderDecoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(horizontal: 30.h, vertical: 16.v),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                      ),
+                      fillColor: appTheme.whiteA700,
                     ),
                   ),
                   SizedBox(height: 10.v),
@@ -117,7 +117,12 @@ class LogInScreen extends StatelessWidget {
                       ),
                       prefixConstraints: BoxConstraints(maxHeight: 50.v),
                       obscureText: true,
-                      borderDecoration: TextFormFieldStyleHelper.outlineBlack,
+                      borderDecoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(horizontal: 30.h, vertical: 16.v),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                        ),
+                      ),
                       fillColor: appTheme.whiteA700,
                     ),
                   ),
