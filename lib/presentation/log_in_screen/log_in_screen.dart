8ffd353d765dc5b62b79c7c9e6671756f88fe1d3
arun12nura <55918280+arun12nura@users.noreply.github.com>
2024-02-21 21:19:31 +0530

    Update log_in_screen.dart

--- lib/presentation/log_in_screen/log_in_screen.dart
+++ lib/presentation/log_in_screen/log_in_screen.dart
@@ -87,14 +87,16 @@ class LogInScreen extends StatelessWidget {
                   Padding(
                     padding: EdgeInsets.symmetric(horizontal: 30.h),
                     child: CustomTextFormField(
-                        controller: mobileController,
-                        hintText: "Mobile",
-                        textInputType: TextInputType.phone,
-                        borderDecoration: InputDecoration(
-                        contentPadding: EdgeInsets.symmetric(horizontal: 30.h, vertical: 16.v),
-                        border: OutlineInputBorder(), // You may need to adjust this
+                       controller: mobileController,
+                       hintText: "Mobile",
+                       textInputType: TextInputType.phone,
+                       borderDecoration: InputDecoration(
+                       contentPadding: EdgeInsets.symmetric(horizontal: 30.h, vertical: 16.v),
+                       border: OutlineInputBorder(
+                       borderSide: BorderSide(color: Colors.black), // Specify the border color
+                         ),
                         ),
-                        fillColor: appTheme.whiteA700,
+                       fillColor: appTheme.whiteA700,
                     ),
                   ),
                   SizedBox(height: 10.v),
